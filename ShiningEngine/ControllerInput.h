d9f7b92823626db69da17368e8fcb9fefca7fe39
BrynDev <bryngamedev@gmail.com>
2020-07-12 13:10:41 +0200

    Reworked the input system, added controller support and removed InputHandlers

---
+++ ShiningEngine/ControllerInput.h
@@ -0,0 +1,29 @@
+#pragma once
+namespace Shining 
+{
+	enum class ControllerInput
+	{
+		ButtonA,
+		ButtonB,
+		ButtonX,
+		ButtonY,
+		DPadUp,
+		DPadRight,
+		DPadDown,
+		DPadLeft,
+		LeftBumper,
+		RightBumper,
+		LeftTrigger,
+		RightTrigger,
+		Select,
+		Start,
+		LeftStickUp,
+		LeftStickRight,
+		LeftStickDown,
+		LeftStickLeft,
+		RightStickUp,
+		RightStickRight,
+		RightStickDown,
+		RightStickLeft,
+	};
+}
